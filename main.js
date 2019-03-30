c9d08f7205e5b73ad6a7424345e71526baebf0c8
bartektenDev <bartek8991@live.com>
2019-03-30 13:08:08 -0500

    Changed behavior of application spawning.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
+const electron = require('electron');
 const path = require('path');
 const url = require('url');
+
+//electron squirrel for release
+// Module to control application life. (this variable should already exist)
+const app2 = electron.app
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent(app2)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
 
 //local Server
 const { fork } = require('child_process')
@@ -36,13 +47,10 @@ function createWindow () {
     mainWindow.loadURL('http://localhost:5000/public/electron/index.html');
   })
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -57,7 +65,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -65,7 +73,68 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) {}
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+};
